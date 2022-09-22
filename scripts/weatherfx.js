b32c34bf491e8fc9dd55805b91cf69ffa3dcfbee
Paulo Roger <me@pauloroger.dev>
2022-09-22 17:54:10 -0300

    small debug helper

--- scripts/weatherfx.js
+++ scripts/weatherfx.js
@@ -19,7 +19,8 @@ Hooks.once("init", () => {
 // Hook that triggers when the game is ready. Check if there is a weather effect been played, then check if sound is enabled and restart the sound that should be played.
 Hooks.once('ready', async function () {
     fvttVersion = parseInt(game.version)
-    console.log("🐺 ==================== FoundryVTT Version: ", fvttVersion)
+    console.log(" ====================================== ⛈ Weather FX  ====================================== ")
+    console.log(" =================================== FoundryVTT Version: ", fvttVersion, " =================================== ")
     //compatibility with v9
     if (fvttVersion < 10) {
         particleWeather = 'fxmaster.updateWeather'
